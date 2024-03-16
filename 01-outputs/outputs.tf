d9c3d9b015a5d0c5559aea70fef3c9636db12f8b
chaitu1 <chaitu143@gmail.com>
2024-03-16 20:44:02 +0530

    push

--- 01-outputs/outputs.tf
+++ 01-outputs/outputs.tf
@@ -1,12 +1,16 @@
-output "name" {
-  value = "I am learning terrroform"
-}
+# output "name" {
+#   value = "I am learning terrroform"
+# }
 
-output "my_name" {
-  value = lookup(var.names, "ram", "helellelle")
-}
+# output "my_name" {
+#   value = lookup(var.names, "ram", "helellelle")
+# }
 
-output "list_name" {
-    value = var.list_name[var.index]
+# output "list_name" {
+#     value = var.list_name[var.index]
   
+# }
+
+output "names" { 
+    value = var.list_name[count.index]
 }

f8e73ec8068f76b81cefacd3f774543cea8d53b0
chaitu1 <chaitu143@gmail.com>
2024-03-16 20:02:27 +0530

    push

--- 01-outputs/outputs.tf
+++ 01-outputs/outputs.tf
@@ -1,3 +1,15 @@
 output "name" {
   value = "I am learning terrroform"
 }
+
+output "my_name" {
+  value = lookup(var.names, chaitu, "ram")
+}
+
+variable "names" {
+    default = {
+      chaitu: "chaithanya",
+      geetha: "geetha_subbarsjss"
+    }
+  
+}

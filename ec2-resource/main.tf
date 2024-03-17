65cd11d8808be015f6acbd4e765dbb354ac2bb97
chaitu1 <chaitu143@gmail.com>
2024-03-17 11:40:31 +0530

    push

--- ec2-resource/main.tf
+++ ec2-resource/main.tf
@@ -6,42 +6,42 @@ provider "aws" {
 resource "aws_instance" "frontend" {
     ami = "ami-05f020f5935e52dc4"
     instance_type = "t3.micro"
-    tags = var.tags.frontend
+    tags = lookup(var.tags,"frontend",{Name: "chaitu", monitor: "yes"})
 }
 
-resource "aws_route53_record" "frontend_record" {
-  zone_id = "Z03998933DKS43BUYGV0L"
-  name    = "frontend.chaithanya.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+# resource "aws_route53_record" "frontend_record" {
+#   zone_id = "Z03998933DKS43BUYGV0L"
+#   name    = "frontend.chaithanya.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.frontend.private_ip]
+# }
 
-resource "aws_instance" "backend" {
-    ami = "ami-05f020f5935e52dc4"
-    instance_type = "t3.micro"
-    tags = var.tags.backend
-}
+# resource "aws_instance" "backend" {
+#     ami = "ami-05f020f5935e52dc4"
+#     instance_type = "t3.micro"
+#     tags = var.tags.backend
+# }
 
-resource "aws_route53_record" "backend_record" {
-  zone_id = "Z03998933DKS43BUYGV0L"
-  name    = "backend.chaithanya.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.backend.private_ip]
-}
+# resource "aws_route53_record" "backend_record" {
+#   zone_id = "Z03998933DKS43BUYGV0L"
+#   name    = "backend.chaithanya.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.backend.private_ip]
+# }
 
-resource "aws_instance" "mysql" {
-    ami = "ami-05f020f5935e52dc4"
-    instance_type = "t3.micro"
-    tags = var.tags.mysql
-}
+# resource "aws_instance" "mysql" {
+#     ami = "ami-05f020f5935e52dc4"
+#     instance_type = "t3.micro"
+#     tags = var.tags.mysql
+# }
 
-resource "aws_route53_record" "mysql_record" {
-  zone_id = "Z03998933DKS43BUYGV0L"
-  name    = "mysql.chaithanya.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
+# resource "aws_route53_record" "mysql_record" {
+#   zone_id = "Z03998933DKS43BUYGV0L"
+#   name    = "mysql.chaithanya.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mysql.private_ip]
+# }
 
--- ec2-resource/outputs.tf
+++ ec2-resource/outputs.tf
@@ -1,23 +1,23 @@
-output "frontend_privateIP" {
-    value = aws_instance.frontend.private_ip
-}
+# output "frontend_privateIP" {
+#     value = aws_instance.frontend.private_ip
+# }
 
-output "backend_privateIP" {
-    value = aws_instance.backend.private_ip
-}
+# output "backend_privateIP" {
+#     value = aws_instance.backend.private_ip
+# }
 
-output "frontend_mysqlIP" {
-    value = aws_instance.mysql.private_ip
-}
+# output "frontend_mysqlIP" {
+#     value = aws_instance.mysql.private_ip
+# }
 
-output "frontend_record_name" {
-    value = aws_route53_record.frontend_record.name
-}
+# output "frontend_record_name" {
+#     value = aws_route53_record.frontend_record.name
+# }
 
-output "backend_record_name" {
-    value = aws_route53_record.backend_record.name
-}
+# output "backend_record_name" {
+#     value = aws_route53_record.backend_record.name
+# }
 
-output "mysql_record_name" {
-    value = aws_route53_record.mysql_record.fqdn
-}
+# output "mysql_record_name" {
+#     value = aws_route53_record.mysql_record.fqdn
+# }

17af9eeab9ef425375bfaec0d52d9d18aff880e8
chaitu1 <chaitu143@gmail.com>
2024-03-17 16:29:25 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -4,11 +4,20 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "iams" {
+    //count = 0
+    //count = length(var.names)
+
+    name =  "ram"
+}
+
+
+resource "aws_iam_user" "iams" {
     count = 0
     //count = length(var.names)
-    for_each = toset(var.names)
-    name =  each.value
+
+    name =  "chaitu"
 }
+
 
 variable "names" {
     default = ["chaitu","geetha","rose","lolll"]

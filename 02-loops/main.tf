068d3e8a39ed995d4bd6bd47f3381f85d1d11389
chaitu1 <chaitu143@gmail.com>
2024-03-17 23:40:10 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -26,12 +26,12 @@ variable "fruits1" {
     }  
 }
 
-# resource "null_resource" "test" {
-#      for_each = var.fruits1 
-#     provisioner "local-exec" {
-#     command = "echo ${lookup(each.value,"name","novalue")}"
-#     } 
-# }
+resource "null_resource" "test" {
+     for_each = var.fruits1 
+    provisioner "local-exec" {
+    command = "echo ${each.value.name} ${each.value.price}"
+    } 
+}
 
 output "f1" {
     value = var.fruits1.name1.name

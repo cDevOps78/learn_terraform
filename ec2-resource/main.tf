94f6f90f91586ade7770474678cd97a25fd94bd4
chaitu1 <chaitu143@gmail.com>
2024-03-17 11:55:08 +0530

    push

--- ec2-resource/main.tf
+++ ec2-resource/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "frontend" {
     ami = "ami-05f020f5935e52dc4"
     instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-037dcd68553894e24","sg-0080caa8626cca9e9"]
     tags = var.tags.frontend.frontend1
 }
 

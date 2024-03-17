72f8b4ac343d4542a7d6959b724650d1ff271c35
chaitu1 <chaitu143@gmail.com>
2024-03-17 11:14:50 +0530

    push

--- ec2-resource/main.tf
+++ ec2-resource/main.tf
@@ -8,3 +8,11 @@ resource "aws_instance" "frontend" {
     instance_type = "t3.micro"
     tags = var.tags
 }
+
+resource "aws_route53_record" "frontend_record" {
+  zone_id = "Z03998933DKS43BUYGV0L"
+  name    = "frontend.chaithanya.online"
+  type    = "A"
+  ttl     = 30
+  records = [data.aws_instances.frontend.private_ips]
+}
--- ec2-resource/outputs.tf
+++ ec2-resource/outputs.tf
@@ -1,3 +1,7 @@
 output "frontend_data_ips" {
-    value = data.aws_instances.frontend.public_ips
+    value = data.aws_instances.frontend.private_ips
+}
+
+output "frontend_record_name" {
+    value = aws_route53_record.frontend_record.name
 }

ee79fcd06a8383c6b5725a45575da5750164f13c
chaitu1 <chaitu143@gmail.com>
2024-03-17 15:00:10 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -1,8 +1,17 @@
 resource "null_resource" "name" {
-    count = length(var.lists)
+    count = length(var.maps)
 }
 
 variable "lists" {
     default = ["chaitru",4,5,true,6]
+  
+}
+
+variable "maps" {
+    default = {
+        name: "name1"
+        names: "name2"
+        name4: "namr5"
+    }
   
 }

5bca3fac8aa54ef4f119fe7c3c2a9c3040cd223a
chaitu1 <chaitu143@gmail.com>
2024-03-18 11:48:39 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -65,3 +65,10 @@ resource "null_resource" "fruits" {
     command = "echo ${var.fruits.fruits1}-${var.fruits.fruits2}"
     } 
 }
+
+resource "null_resource" "fruit1" {
+     for_each = var.fruits
+    provisioner "local-exec" {
+    command = "echo ${var.fruits.fruits1}-${var.fruits.fruits2}"
+    } 
+}

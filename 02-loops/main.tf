a066248c47aa0523ed467a11109ca955f2e87e37
chaitu1 <chaitu143@gmail.com>
2024-03-18 19:38:22 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -90,7 +90,7 @@ variable "fruits" {
 #     } 
 # }
 
-resource "null_resource" "test3" {
+resource "null_resource" "test4" {
      for_each = var.fruits
     provisioner "local-exec" {
     command = "echo ${var.fruits[each.key]}"

02baea7c6160b53694d7528e222a1fa8c2b7e969
chaitu1 <chaitu143@gmail.com>
2024-03-18 11:31:49 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -1,45 +1,61 @@
 
+# variable "fruits" {
+#     default = {
+#         apple: 10,
+#         orange: 500
+#         banana: 20
+
+#     }  
+# }
+
+# variable "fruits1" {
+#     default = {
+#         name1 = {
+#         name: "apple"
+#         price: 20
+#         }
+#        name2 = {
+#         name: "banababab"
+#         price: 60000
+#        } 
+#        name3 = {
+#         name: "banababab"
+#         price: 60000
+#        }
+
+#     }  
+# }
+
+# resource "null_resource" "test" {
+#      for_each = var.fruits1 
+#     provisioner "local-exec" {
+#     command = "echo ${each.value.name} ${each.value.price}"
+#     } 
+# }
+
+# output "f1" {
+#     value = var.fruits1.name1
+  
+# }
+
+
+# output "f2" {
+#     value = var.fruits1.name2
+  
+# }
+
+
 variable "fruits" {
     default = {
-        apple: 10,
-        orange: 500
-        banana: 20
-
-    }  
+        banana: 10
+        orange: 20
+    }
+  
 }
 
-variable "fruits1" {
-    default = {
-        name1 = {
-        name: "apple"
-        price: 20
-        }
-       name2 = {
-        name: "banababab"
-        price: 60000
-       } 
-       name3 = {
-        name: "banababab"
-        price: 60000
-       }
-
-    }  
-}
-
-resource "null_resource" "test" {
-     for_each = var.fruits1 
+resource "null_resource" "fruits" {
+     for_each = var.fruits
     provisioner "local-exec" {
-    command = "echo ${each.value.name} ${each.value.price}"
+    command = "echo ${each.key} = ${each.value}"
     } 
-}
-
-output "f1" {
-    value = var.fruits1.name1
-  
-}
-
-
-output "f2" {
-    value = var.fruits1.name2
-  
 }

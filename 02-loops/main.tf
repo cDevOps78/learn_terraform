0821e6a52607c5cd6c4f088e15db14645b506090
chaitu1 <chaitu143@gmail.com>
2024-03-18 12:42:10 +0530

    push

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -97,12 +97,6 @@ variable "fruits" {
 #     } 
 # }
 
-resource "null_resource" "test4" {
-     for_each = var.fruits
-    provisioner "local-exec" {
-    command = "echo ${var.fruits[each.value]["apple"]}"
-    } 
-}
 
 
 

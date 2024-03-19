3fee8891a5df889ff09cf8c156089e7a2f53ccbc
chaitu1 <chaitu143@gmail.com>
2024-03-19 15:53:21 +0530

    push

--- expense-project-v1/outputs.tf
+++ expense-project-v1/outputs.tf
@@ -11,3 +11,10 @@ output "data_instances" {
 output "length_instances" {
   value = length(data.aws_instances.data_instances.private_ips)
 }
+
+resource "null_resource" "fruit0" {
+    for_each = length(data.aws_instances.data_instances.private_ips)
+    provisioner "local-exec" {
+    command = "echo ${each.key}"
+    } 
+}

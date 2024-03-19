cd76df95ae5f0e5fb84ec4881f941347892e834b
chaitu1 <chaitu143@gmail.com>
2024-03-19 15:22:04 +0530

    push

--- expense-project-v1/main.tf
+++ expense-project-v1/main.tf
@@ -1,8 +1,13 @@
-# resource "aws_instance" "name" {
-#     instance_type = "t2.micro"
-#     ami = "ami-05f020f5935e52dc4"
-#     tags = 
-# }
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "frontend" {
+    instance_type = "t2.micro"
+    ami = "ami-05f020f5935e52dc4"
+    tags = lookup(var.tags, "frontend","novalue")
+}
 
 # -----------------------IMP-----------------------------------------
 # output "tags" {

b86e3d3bcaf0cade437c3d17822be06be8ad8fca
chaitu1 <chaitu143@gmail.com>
2024-03-19 15:26:41 +0530

    push

--- expense-project-v1/main.tf
+++ expense-project-v1/main.tf
@@ -9,6 +9,11 @@ resource "aws_instance" "frontend" {
     tags = lookup(var.tags, "frontend","novalue")
 }
 
+resource "aws_instance" "mysql" {
+    instance_type = "t2.micro"
+    ami = "ami-05f020f5935e52dc4"
+    tags = lookup(var.tags, "mysql","novalue")
+}
 # -----------------------IMP-----------------------------------------
 # output "tags" {
 #   value = var.tags.frontend.Name

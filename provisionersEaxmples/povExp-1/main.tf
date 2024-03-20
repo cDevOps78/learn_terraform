3a1637e88921771f4fb1629f45c9d7a9e07b8097
chaitu1 <chaitu143@gmail.com>
2024-03-20 20:24:53 +0530

    push

--- provisionersEaxmples/povExp-1/main.tf
+++ provisionersEaxmples/povExp-1/main.tf
@@ -4,28 +4,55 @@ provider "aws" {
     region      = "us-east-1"  
 }
 
- resource "aws_instance" "nginx" {
-    ami           = data.aws_ami.ami_id.id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-037dcd68553894e24"]
-    tags          = lookup(var.tags,"nginx","notags")
+#  resource "aws_instance" "nginx" {
+#     ami           = data.aws_ami.ami_id.id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = ["sg-037dcd68553894e24"]
+#     tags          = lookup(var.tags,"nginx","notags")
 
-    provisioner "remote-exec" {
+#     provisioner "remote-exec" {
+
+#          connection {
+#          type     = "ssh"
+#          user     = "ec2-user"
+#          password = "DevOps321"
+#          host     = self.public_ip
+#         }
+
+#         inline = [ 
+#             "sudo dnf install nginx -y",
+#             "sudo rm -rf /usr/share/nginx/html/*",
+#             "sudo echo Hello-chaitu  > /usr/share/nginx/html/index.html",
+#             "sudo systemctl start nginx"
+#          ]
+#     }
+#  }
+
+ ########## with decouple of Provisioner #######
+
+resource "aws_instance" "nginx" {
+    ami                    = data.aws_ami.ami_id.id
+    instance_type          = "t3.micro"
+    vpc_security_group_ids = ["sg-037dcd68553894e24"]
+    tags                   = lookup(var.tags,"nginx","notags")
+
+}    
+
+resource "null_resource" "nginx_provisioner" {
+        provisioner "remote-exec" {
 
          connection {
          type     = "ssh"
          user     = "ec2-user"
          password = "DevOps321"
-         host     = self.public_ip
+         host     = aws_instance.nginx.public_ip
         }
 
         inline = [ 
-            "sudo dnf install nginx -y",
+            "dnf install nginx -y",
             "sudo rm -rf /usr/share/nginx/html/*",
-            "sudo echo Hello-chaitu  > /usr/share/nginx/html/index.html",
             "sudo systemctl start nginx"
          ]
     }
- }
-
- # with decouple of Provisioner 
+}
+  

2baea0251998a7baa4d38237048730cda36215e8
chaitu1 <chaitu143@gmail.com>
2024-03-24 19:32:06 +0530

    practing multiples time

--- 02-outputs/main.tf
+++ 02-outputs/main.tf
@@ -3,16 +3,18 @@ output "name" {
 }
 
 output "map" {
- // value = lookup(var.map,"name2","novalus")
+ // value = lookup(var.map,"name2","novalus") [ Or ]
   value = var.map.name2
 }
 
 output "nested_map_movie" {
-  value = var.nested_map.movies.movie1
+  // value = var.nested_map.movies.movie1  [ Or ]
+  value = var.nested_map.movies["movie2"]
 }
 
 output "nested_map_song" {
-  value = var.nested_map.songs["song3"]
+  // value = var.nested_map.songs["song3"] [ Or ]
+  value = lookup(var.nested_map.songs, "song3" ,"nodefault")
 }
 
 

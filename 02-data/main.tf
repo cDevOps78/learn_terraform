52d8cd2b890f80e1afe454d9aa0df5edd246cccd
chaitu1 <chaitu143@gmail.com>
2024-03-24 21:06:01 +0530

    working with data sources

--- 02-data/main.tf
+++ 02-data/main.tf
@@ -15,16 +15,16 @@ provider "aws" {
 #}
 
 
-#resource "aws_key_pair" "chaitu" {
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDI7aU6xaXo2ZfHDpVZgiM5YJe1xKQZhgfxM2coBT8fPzHB5HwTW2JmyJkgyTTcm6P7XHmcPfnYhZzqvDCZRsG4zXvdj/6Y1Ye50wFaVysVs7els+lFp88nOoL+IcTQuXvbnLADOn2Y28r0K4qvxXImVxVVewVpKESudiPLGgMOsNN34oFe/7hLgrbetVnw0SfGrS9F8d9cZAsIIOP6PZe5AKOcarKYKvrpSMVNBshUNiGOjyw15+pM7PmGz+jT5fis5T2/flB0NpMnMscswjv2to5ihISWAlUAInkHDAOQSk4sz8/LkHRYtqt4vyUBofzf34WuV+P9oCr+8pU3QLy6BNicXBxJz6k3IZ3OEhIQI4dKfbzpczh94tORFOx90Lpgi24XtCr5NogKq43M6kZr8pkWPiKSCD/P/JeFOzax8APqe6nY7FU7hEMcFlG1FmFFH9QB4rVUeg1rskzhel0NQ4mbncYknOy220WxyS36BE+Sn6lrLkrpOYyAlHMAwms= ec2-user@ip-172-31-9-213.ec2.internal"
-#  key_name = "chaitu"
+resource "aws_key_pair" "chaitu" {
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDI7aU6xaXo2ZfHDpVZgiM5YJe1xKQZhgfxM2coBT8fPzHB5HwTW2JmyJkgyTTcm6P7XHmcPfnYhZzqvDCZRsG4zXvdj/6Y1Ye50wFaVysVs7els+lFp88nOoL+IcTQuXvbnLADOn2Y28r0K4qvxXImVxVVewVpKESudiPLGgMOsNN34oFe/7hLgrbetVnw0SfGrS9F8d9cZAsIIOP6PZe5AKOcarKYKvrpSMVNBshUNiGOjyw15+pM7PmGz+jT5fis5T2/flB0NpMnMscswjv2to5ihISWAlUAInkHDAOQSk4sz8/LkHRYtqt4vyUBofzf34WuV+P9oCr+8pU3QLy6BNicXBxJz6k3IZ3OEhIQI4dKfbzpczh94tORFOx90Lpgi24XtCr5NogKq43M6kZr8pkWPiKSCD/P/JeFOzax8APqe6nY7FU7hEMcFlG1FmFFH9QB4rVUeg1rskzhel0NQ4mbncYknOy220WxyS36BE+Sn6lrLkrpOYyAlHMAwms= ec2-user@ip-172-31-9-213.ec2.internal"
+  key_name = "chaitu"
+}
+
+#data "aws_key_pair" "example" {
+#  key_name           = "chaitu"
 #}
-
-data "aws_key_pair" "example" {
-  key_name           = "chaitu"
-}
-
-output "aws_key_pair" {
-  value = data.aws_key_pair.example.public_key
-}
+#
+#output "aws_key_pair" {
+#  value = data.aws_key_pair.example.public_key
+#}
 

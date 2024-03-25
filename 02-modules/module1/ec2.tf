4be5756d5513b70eb6472432bc20fc237f458d96
chaitu1 <chaitu143@gmail.com>
2024-03-25 13:51:40 +0530

    modules

--- 02-modules/module1/ec2.tf
+++ 02-modules/module1/ec2.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
+  tags = {
+    Name = var.Name
+  }
+}
+
+variable "Name" {
+  default = "devv"
 }

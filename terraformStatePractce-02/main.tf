4cd596b28174cf2539f235a8bbca80ef2623c59d
chaitu1 <chaitu143@gmail.com>
2024-03-25 22:28:15 +0530

    terraform state practice

---
+++ terraformStatePractce-02/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+   backend "s3" {
+     bucket = "b78-chaitu"
+     key    = "dev/state"
+     region = "us-east-1"
+   }
+ }
+
+variable "name" {
+  default = "chaithnaya"
+}
+
+output "name" {
+  value = var.name
+}

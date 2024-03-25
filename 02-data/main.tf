3953f12fe3818b9d5dabc24d37dcd5e2a1205771
chaitu1 <chaitu143@gmail.com>
2024-03-25 08:57:31 +0530

    data source for security group

--- 02-data/main.tf
+++ 02-data/main.tf
@@ -1,7 +1,7 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#
+provider "aws" {
+  region = "us-east-1"
+}
+
 ## Data source to security-groups
 #data "aws_security_groups" "test" {
 #  tags = {

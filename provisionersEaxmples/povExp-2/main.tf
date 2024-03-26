75597bae80878bfde2a6c5ec6e1ead2e907ebaa0
chaitu1 <chaitu143@gmail.com>
2024-03-26 17:17:53 +0530

    practing provisioner part-2

--- provisionersEaxmples/povExp-2/main.tf
+++ provisionersEaxmples/povExp-2/main.tf
@@ -1,30 +1,38 @@
+variable "instances" {
+  default = {
+    frontend = "Frontend"
+    backend = "Backend"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "nginx" {
+  for_each = var.instances
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-037dcd68553894e24"]
+  tags = {
+    Name = lookup(var.instances,each.key,"nokey")
+  }
 }
 
 resource "null_resource" "nginxf" {
+  for_each = var.instances
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
       user     = "ec2-user"
       password = "DevOps321"
-      host     = aws_instance.nginx.private_ip
+      host     = lookup(aws_instance.nginx,each.key,"novalue").private_ip
     }
     inline = [
             "sudo dnf install nginx -y",
-            "sudo rm -rf /usr/share/nginx/html/* ",
-            "echo 'hello-chaitu12333' > /usr/share/nginx/html/index.html",
             "sudo systemctl start nginx"
     ]
   }
 }
 
-output "nginxPublicIP" {
-  value = aws_instance.nginx.public_ip
-}

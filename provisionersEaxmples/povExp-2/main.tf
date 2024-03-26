4c18ac7c428e1b4ef082b5b1ed6a81e6a15d5e27
chaitu1 <chaitu143@gmail.com>
2024-03-26 17:05:51 +0530

    practing provisioner part-2

--- provisionersEaxmples/povExp-2/main.tf
+++ provisionersEaxmples/povExp-2/main.tf
@@ -19,8 +19,12 @@ resource "null_resource" "nginxf" {
     inline = [
             "sudo dnf install nginx -y",
             "sudo rm -rf /usr/share/nginx/html/* ",
-            "echo 'hello-chaitu' > /usr/share/nginx/html/index.html",
+            "echo 'hello-chaitu12333' > /usr/share/nginx/html/index.html",
             "sudo systemctl start nginx"
     ]
   }
+}
+
+output "nginxPublicIP" {
+  value = aws_instance.nginx.public_ip
 }
